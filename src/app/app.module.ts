a60c1e3302f35f5f0fac1fc64fed7effdbc0df62
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-11-29 09:59:22 +0100

    add homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Inject } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import {NgbModule, NgbTabsetModule, NgbAccordionModule} from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 import { REMIX } from './remix';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { environment } from '../environments/environment';
+// components
+import { AppComponent } from './app.component';
 import { ListComponent } from './client-manager/list/list.component';
 import { ViewComponent } from './client-manager/view/view.component';
 import { ClientManagerActiveGuard } from './client-manager/active.guard';
 
+import {NgbModule, NgbTabsetModule, NgbAccordionModule} from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    ViewComponent
+    ViewComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([{
       path: '',
+      component: ListComponent,
+    }, {
+      path: 'list',
       component: ListComponent,
     }, {
       path: ':name',
